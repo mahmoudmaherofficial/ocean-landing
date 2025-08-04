5a87ac61336e0782daccc3b1dd8e99d84efc997f
Mahmoud Maher <mahmoudmaherofficial@gmail.com>
2025-08-04 13:19:26 +0300

    removed: Types Errors

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,14 +3,20 @@ import { FC, ReactNode } from 'react';
 interface ButtonProps {
   variant?: 'primary' | 'secondary' | 'outline';
   children: ReactNode;
+  isFullSize?: boolean;
 }
 
 const Button: FC<ButtonProps> = ({
   variant = 'primary',
   children,
+  isFullSize = false,
 }) => {
   return (
-    <button className={`btn ${variant && `btn-${variant}`}`}>{children}</button>
+    <button
+      className={`btn ${variant && `btn-${variant}`} ${isFullSize && 'w-full'}`}
+    >
+      {children}
+    </button>
   );
 };
 
--- components/FeatureBlock.tsx
+++ components/FeatureBlock.tsx
@@ -3,9 +3,7 @@ import Image from 'next/image';
 import { FC } from 'react';
 import Heading from '@/components/Heading';
 
-interface FeatureBlockProps extends featureBlock {}
-
-const FeatureBlock: FC<FeatureBlockProps> = ({
+const FeatureBlock: FC<featureBlock> = ({
   title,
   imageUrl,
   content,
